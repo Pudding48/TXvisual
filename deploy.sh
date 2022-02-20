3b77ea94b6afeeedf0735990bd5e0840fdeff4ae
Ryoma Suzuki <s1811425@s.tsukuba.ac.jp>
2022-02-20 15:53:24 +0900

    deploy

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+
+git init
+git add -A
+git commit -m 'deploy'
+#git push -f git@github.com/Pudding48/palestine.git master:gh-pages
+git push -f https://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@github.com/Pudding48/TXvisual.git origin
+cd -
