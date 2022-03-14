d20c7b07be5bace515f3db7abfa6723382fd02a6
Ryoma Suzuki <s1811425@s.tsukuba.ac.jp>
2022-03-14 15:42:43 +0900

    added train data to timetable

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ request.onload = function(){
         request.send();
         request.onload = function(){
             timeTables = request.response;
-            setTrainOnOrigin();
+            // setTrainOnOrigin();
             trainAnimation();
         }
     }
@@ -37,7 +37,7 @@ var moveTrain;
 function setTrainOnOrigin(){ //sets train on origin point
     let trainTable = timeTables.trains[0];
     let train = document.getElementsByClassName("train");
-    
+
     var origin_ratio = getStation(trainTable.origin);
     setPosition(train[0], origin_ratio.position);
 }
@@ -51,35 +51,65 @@ function trainAnimation(){
         if(currTime > maxTime){
             currTime = 0;
         }
-        let ratio = currTime / 100;
-        var logging = document.getElementById("logging");
-        logging.innerHTML = ratio;
-        // console.log(ratio);
-        
-        setPosition(train[0], ratio);
+
+        var flag = setTrainStatus(currTime);
+        if(flag == "move"){
+            setPosition(train[0], currTime);
+        } if(flag == "stop") {
+            train[0].style.display = "none";
+        } else {
+            train[0].style.display = "block";
+        }
+
     }, 50);
 }
 
-function setTrainStatus(obj, ratio){
-    let table = timeTables.trains[0];
-    var destination_pos = getStation(table.destination).position;
-    if(ratio > destination_pos){
-        
+function setTrainStatus(currTime){
+    const arrival = timeTables.trains[0].table.filter(
+        function(target){
+            return target.id == timeTables.trains[0].destination;
+        }
+    )
+    
+    if(currTime > arrival[0].arrival){
+        return "stop";
+    } else {
+        return "move";
     }
 }
 
-function setPosition(target, ratio){ //puts target on position based on ratio
+function setPosition(target, currTime){ //puts target on position based on ratio
+    let origin = getStation(timeTables.trains[0].origin);
+    let destination = getStation(timeTables.trains[0].destination);
+    
+    var time_ratio = ratioByTime(currTime);
+    var ratio = time_ratio * (destination.position - origin.position) + origin.position;
+    ratio = ratio / 100;
+
+    let bboxrec = target.getBBox();
+
+    //puts object on point defiend by ratio * total_length
     var line = document.getElementById("TX_line");
     var total_length = line.getTotalLength();
-    let bboxrec = target.getBBox();
     let position = line.getPointAtLength(ratio * total_length);
     position.x -= bboxrec.width/2;
     position.y -= bboxrec.height/2;
     target.setAttribute("transform", "translate("+position.x+","+position.y+")");
 }
 
-function toggle(){
-    clearInterval(moveTrain);
+function ratioByTime(currTime){//returns how much time has passed within the time set in the time table
+    const destination = timeTables.trains[0].table.filter(
+        function(target){
+            return target.id == timeTables.trains[0].destination;
+        }
+    )
+    const origin = timeTables.trains[0].table.filter(
+        function(target){
+            return target.id == timeTables.trains[0].origin;
+        }
+    )
+    
+    return currTime / (destination[0].arrival - origin[0].departure);
 }
 
 function getStation(el){ //returns station data based on id
@@ -89,4 +119,13 @@ function getStation(el){ //returns station data based on id
         }
     );
     return result[0];
+}
+
+function toggle(){
+    clearInterval(moveTrain);
+}
+
+function logText(object){
+    var logging = document.getElementById("logging");
+    logging.innerHTML = object;
 }
